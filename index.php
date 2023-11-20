a2e75de5d1cc45fd15e9e9ad546884f40cbbe084
Chris20033 <capimarquez23@gmail.com>
2023-11-19 19:49:43 -0600

    Chris20033

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <section class="container">
-             
-           
+    <section class="container"> 
+        <!-- PEZ -->
           <img src="./imgGit/1SPRITE.png" alt=""> 
           <img src="./imgGit/2SPRITE.png" alt=""> 
           <img src="./imgGit/3SPRITE.png" alt=""> 
-          <img src="./imgGit/5SPRITE.png" alt=""> 
+          <img src="./imgGit/5SPRITE.png" alt="" id="pezImagen">   
           
+          <!-- boton -->
+          <button>
+            <img src="./imgGit/1button.png" alt="" id="imagenBoton">
+          </button>
     </section>
    
     
